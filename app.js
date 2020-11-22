ad1acd6b72810f59e7e6ee20af9517678804c6ec
Saleh AlQattan <SalehAlQattan@outlook.com>
2020-11-22 23:14:42 +0300

    app and list modified

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { render } = require('ejs');
 const app = express();
 
 let items = ['Buy Food', 'Cook Food', 'Eat Food'];
-
 let workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
-
 app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-
     let today = new Date();
-
     let options = {
         weekday: 'long',
         day: 'numeric',
         month: 'long'
     };
-
-
     let day = today.toLocaleDateString('en-US', options);
-
-
     res.render('list', { listTitle: day, newListItems: items });
 });
 
 
 app.post('/', (req, res) => {
-
-
     let item = req.body.newItem;
-
     if (req.body.list === 'Work list') {
         workItems.push(item);
         res.redirect('/work');
@@ -46,17 +35,15 @@ app.post('/', (req, res) => {
 
 });
 
+
+
 app.get('/work', (req, res) => {
     res.render('list', { listTitle: 'Work list', newListItems: workItems });
 });
 
-
 app.post('/work', (req, res) => {
-
     let item = req.body.newItem;
-
     workItems.push(item);
-
     res.redirect('/work');
 });
 
